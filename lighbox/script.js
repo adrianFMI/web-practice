6248dc2da5c3ff1d5fed918948d61e6d7fc1f553
adrianFMI <47193253+adrianFMI@users.noreply.github.com>
2019-03-15 12:04:17 -0700

    update 15/03/2019

--- lighbox/script.js
+++ lighbox/script.js
@@ -1,24 +1,30 @@
 "use strict"
 console.log("cheking");
 $(document).ready(function () {
-    var img1 = $("#image1");
-    var lightbox = $("#lightbox");
     var close = $("#close");
     var imgs = ["thumbnail.jpg", "sea-thumbnail.jpg", "thumbnail.jpg"];
-    img1.click(function () {
-        lightbox.css("width", "100%");
-        lightbox.css("height", "100%");
-        lightbox.css("background-color", "rgba(0, 0 ,0 ,.75)");
-        lightbox.css("display", "block");
-    });
+    var bigImg = ["wall-animal-dog-pet.jpg","sea.jpeg","wall-animal-dog-pet.jpg"];
     close.click(function () {
         lightbox.css("display", "none");
     });
 
     for (let i = 0; i < imgs.length; i++) {
-        $("body").append("<img id='image" + i + "' class='gallery' src='images/" + imgs[i] + "'>");
-        $("#image" + i).click(function () {
-            console.log("clicked " + i);
+        var imgId = "image" + i;
+        //$("body").append("<img id='" + imgId + "' class='gallery' src='images/" + imgs[i] + "'>");
+        $("body").append(`<img id='${imgId}' class='gallery' src='images/${imgs[i]}' alt='images'>`);
+        $("#" + imgId).click(function () {
+            $("body").append("<div id='lightbox'></div>");
+            var lightbox = $("#lightbox");
+            lightbox.css("width", "100%");
+            lightbox.css("height", "100%");
+            lightbox.css("background-color", "rgba(0, 0 ,0 ,.75)");
+            lightbox.css("display", "block");
+            
+            lightbox.append(`<img src='images/${bigImg[i]}' alt='images'>`);
+
+            lightbox.click(function () {
+                lightbox.remove();
+            });
         });
     }
 
